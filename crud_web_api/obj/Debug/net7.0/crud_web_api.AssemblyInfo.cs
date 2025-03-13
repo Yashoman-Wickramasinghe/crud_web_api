6c430db99dc6c7649f7709714bf415fc117b57ec
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-03-13 23:55:36 +1100

    Add EmployeeController and implement Add, Edit, Delete, Update employees. Get employee by Id.

--- crud_web_api/obj/Debug/net7.0/crud_web_api.AssemblyInfo.cs
+++ crud_web_api/obj/Debug/net7.0/crud_web_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crud_web_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdbcdd4ed2571ca8192cb2db0a187e6d1870b36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ec4fd3516221fcfea1b5b9bedcc868c1cd6466")]
 [assembly: System.Reflection.AssemblyProductAttribute("crud_web_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crud_web_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
