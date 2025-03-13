d0ec4fd3516221fcfea1b5b9bedcc868c1cd6466
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-03-13 21:42:56 +1100

    Initial commit

---
+++ crud_web_api/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace crud_web_api.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public int isActive {  get; set; }
+    }
+}
